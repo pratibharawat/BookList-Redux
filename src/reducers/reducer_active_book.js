ad5b97d2ad3a5cfc45a27a561ef6c2c26a684f0e
Pratibha Rawat <prawat@amazon.com>
2018-03-23 16:40:59 -0700

    book list- react and redux

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,5 +1,5 @@
 //State argument is not the application state, but the state which this reducer is responsible for
-export default function(state = null, action){
+export default function(state=null, action){
   switch (action.type) {
     case 'BOOK_SELECTED':
       return action.payload;  //never mutate the current state
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
   return [
-    {title: 'Javascript: The Good Parts'},
-    {title: 'Harry Potter'},
-    {title: 'Tower'},
-    {title: 'Ruby'}
+    {title: 'Javascript: The Good Parts', pages: 160},
+    {title: 'Harry Potter', pages: 60},
+    {title: 'Tower', pages: 100},
+    {title: 'Ruby', pages: 10}
   ];
 }
